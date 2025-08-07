8260c0273aa9b3d603dbcebf24adc94dec51c3da
zotilrk <krloz@Juans-MacBook-Pro.local>
2025-08-06 21:06:04 -0600

    Crea directorio semana 3

---
+++ Semana 3/CrudMongoDb/src/main/java/com/jcbs/crud/mongodb/CrudMongoDb/CrudMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.jcbs.crud.mongodb.CrudMongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMongoDbApplication.class, args);
+	}
+
+}
